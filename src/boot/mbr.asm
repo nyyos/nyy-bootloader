99a0e7f6888506b67dc935ada853fac5c437b984
bomba-cat <bledion.morina@hotmail.com>
2024-09-26 22:00:07 +0200

    removed partition table and message logs

--- src/boot/mbr.asm
+++ src/boot/mbr.asm
@@ -26,7 +26,7 @@
 
 .entry:
     ; Entry for First Stage Bootloader, its only goal is to
-    ; load the Second Stage Bootloader and going to 32bit mode.
+    ; load the Second Stage Bootloader and going to 64bit mode.
     ; Also keep in mind this Bootloader is not designed for
     ; all types of hardware and firmwares so R.I.P my
     ; hard work and labour
@@ -37,38 +37,24 @@
 
     mov 	  ss, ax
     mov 	  sp, 0x7C00
-.entrymessage:
-    mov 	  si, Entered
-    call 	  .echo
-.failuremessage:
-    mov 	  si, Failure
+
+; ------Bad-Stuff------
+.failmessage:
+    mov 	  si, Fail
     call	  .echo
 .hlt:
     hlt
     jmp		  .hlt
+; ---------------------
 
-; Define Variables
-    Entered: 
-            db '[INFO] Entered Stage1 Bootloading', NEXL, 0
-    Entering:
-            db '[INFO] Entering Stage2 Bootloading', NEXL, 0
-    Failure:
+; ---Define-Variables--
+    Fail:
             db '[CRITICAL] Entering Stage2 Bootloading failed', NEXL, 0
+; ---------------------
 
-; 446 bytes
+; 446 bytes, start of partition table (64 bytes overall, 16 bytes each)
 times 446-($-$$) db 0
 
-; Partition Table 64 bytes total
-
-; Partition 1 (ext2 Partition)
-.partitiontable:
-        db 0x80                ; Bootable flag (0x80 = active)
-        db 0x01, 0x01, 0x00    ; Starting CHS (legacy BIOS ignored, safe to set like this)
-        db 0x83                ; Partition type (0x83 = ext2/ext3/ext4)
-        db 0xFE, 0xFF, 0xFF    ; Ending CHS (legacy BIOS ignored)
-        dd 0x0000003F          ; Starting sector (LBA 63)
-        dd 0x1DBE7BF           ; Partition size in sectors (15GB example)
-
-; Remaining 3 partitions (16 bytes each)
-times 64-16 db 0
-        dw 0xAA55
+; Remaining 4 partitions
+times 64 db 0
+        dw 0xAA55 ; MBR Boot Signature
